0f6381b6975ea39e02360aff441caeabf57c9208
jyothimukala555 <138197142+jyothimukala555@users.noreply.github.com>
2023-08-02 23:37:58 +0530

    Create IntegersList.java

---
+++ IntegersList.java
@@ -0,0 +1,26 @@
+package com.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IntegersList {
+	
+	public static void main (String [] args) {
+	
+		List <Integer> list = new ArrayList<Integer>();
+		
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(4);
+		list.add(5);
+		list.add(6);
+		list.add(7);
+		list.add(8);
+		list.add(9);
+		
+		System.out.println("List = "+ list);
+		list.remove(5);
+		System.out.println("List = "+ list);
+	}
+	}
